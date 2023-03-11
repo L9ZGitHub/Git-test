e6215311ab7884e595d38b269ede222b2778d53e
l9zyr <l9zyr@163.com>
2023-03-11 09:19:02 +0800

    20230311-2 commit

--- Git-1/src/main/java/edu/lzu/git/Hello.java
+++ Git-1/src/main/java/edu/lzu/git/Hello.java
@@ -1,9 +1,9 @@
 package edu.lzu.git;
 
-
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello git");
+
         System.out.println("20230311-1 commit");
+        System.out.println("20230311-2 commit");
     }
 }
